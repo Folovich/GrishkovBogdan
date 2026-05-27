c100892dd5689a37f464d2a91a325f76e28b6108
Perevozchikov NS <student@school.ru>
2026-05-27 18:28:30 +0300

    C2: Добавлен ввод двух целых чисел

--- Downloads/main.cpp
+++ Downloads/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 int main() {
     std::cout << "Программа запущена" << std::endl;
+    int a, b;
+    std::cout << "Введите первое целое число: ";
+    std::cin >> a;
+    std::cout << "Введите второе целое число: ";
+    std::cin >> b;
     return 0;
 }
