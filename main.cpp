2e1e5c2e629b5209a7a4723b90f9abcff7164caf
StevegetZZZ <dimamalkin2007@gmail.com>
2026-05-27 13:27:54 +0300

    C2: Add input for two integers

--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@
 
 int main() {
     std::cout << "Программа запущена" << std::endl;
+    
+    int a, b;
+    std::cout << "Введите первое целое число: ";
+    std::cin >> a;
+    std::cout << "Введите второе целое число: ";
+    std::cin >> b;
+    
     return 0;
 }
